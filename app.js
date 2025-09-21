67bc807cd8e810c6bcd9ec83b5db9d2d96947c6c
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-09-21 01:35:44 -0500

    added some more tweaks

--- app.js
+++ app.js
@@ -4,18 +4,29 @@ require("dotenv").config();
 
 const express = require("express");
 const ejs = require("ejs");
+const cookieParser = require("cookie-parser");
 
 const colors = require("colors");
 
 const path = require("path");
 const fs = require("fs");
 
-let db = require("./db")
-db = new db(path.join(__dirname,"site.db"))
+let db = require("./db");
+db = new db(path.join(__dirname,"site.db"));
+
+const Auth = require("@austinsdk/auth");
+const { error } = require("console");
+const auth = new Auth(
+    process.env.auth_server_url,
+    process.env.client_id,
+    process.env.client_secret,
+    process.env.callback
+)
 
 // setup
 
 let app = express();
+app.use(cookieParser());
 
 // epic (static) endpoints
 
@@ -57,6 +68,25 @@ app.get("/u/:short",(req,res,next)=>{
         return res.send("extra check")
     }
     res.redirect(_short.url)
+})
+
+// cool login stuff ig
+app.get("/login",(req,res,next)=>{
+    const authUrl = auth.getAuthUrl();
+    return res.redirect(authUrl)
+})
+app.get("/callback/auth",async (req,res,next)=>{
+    let _code = req.query.code;
+    let tokenResponse;
+    try {
+        tokenResponse = await auth.codeForToken(_code);
+        if (!tokenResponse.access_token) throw error("invalid auth?")
+        res.cookie("token",tokenResponse.access_token);
+        res.send("check cookies?")
+    } catch (error) {
+        console.log('Oops, something went wrong:', error);
+        res.send(error,":(")
+    }
 })
 
 // listen
