f0d3712aad1e8ec2ccbf05b325455aa030c373dd
Austin Moore <133458852+Null-Austin@users.noreply.github.com>
2025-09-21 00:50:13 -0500

    cool inital db thingy

--- db.js
+++ db.js
@@ -1,6 +1,21 @@
 const sqlite3 = require("better-sqlite3");
 module.exports = class{
+    init(){
+        this.db.exec(`
+            CREATE TABLE IF NOT EXISTS short_urls (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            short TEXT UNIQUE NOT NULL,
+            url TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            warning BOOLEAN NOT NULL DEFAULT false,
+            username TEXT NOT NULL DEFAULT "no-name"
+            );
+        `);
+    }
     constructor(path){
         this.db = sqlite3(path);
+        this.shortens = {};
+        this["not-shortens"] = {}; // the exact opposite as of above :hs:
     }
+    
 }
